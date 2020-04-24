7c2c4bb5128acb266d779213a64b9f865af1f10b
NyaMW <renjieli.10@gmail.com>
2020-04-23 23:43:17 -0400

    Purify jobs data returned from GitHub Job API to only have the information needed

--- jupiter/src/main/java/rpc/SearchItem.java
+++ jupiter/src/main/java/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import entity.Item;
 import external.GitHubClient;
 
 
@@ -32,26 +34,16 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.setContentType("application/json");
-//		PrintWriter writer = response.getWriter();
-//		if (request.getParameter("username") != null) {
-//			JSONObject obj = new JSONObject();
-//			String username = request.getParameter("username");
-//			obj.put("username", username);
-//			writer.print(obj);
-//		}
-//		
-//		JSONArray array = new JSONArray();
-//		array.put(new JSONObject().put("username", "abcd"));
-//		array.put(new JSONObject().put("username", "1234"));
-////		writer.print(array);
-//		RpcHelper.writeJsonArray(response, array);
-		
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 
 		GitHubClient client = new GitHubClient();
-		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
+		List<Item> items = client.search(lat, lon, null);
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		RpcHelper.writeJsonArray(response, array);
 	}
 
 	/**
