3a89a6d960d7a00a7435e0cc5da56363516fa6ac
NyaMW <renjieli.10@gmail.com>
2020-04-30 23:39:43 -0400

    impl new methods to read/write user data from/to databse + create login/logout/register servlets to serve user requests

--- jupiter/src/main/java/db/MySQLConnection.java
+++ jupiter/src/main/java/db/MySQLConnection.java
@@ -11,7 +11,6 @@ import java.util.Set;
 import entity.Item;
 import entity.Item.ItemBuilder;
 
-
 public class MySQLConnection {
 	private Connection conn;
 
@@ -33,7 +32,7 @@ public class MySQLConnection {
 			}
 		}
 	}
-	
+
 	public void setFavoriteItems(String userId, Item item) {
 		if (conn == null) {
 			System.err.println("DB connection failed");
@@ -81,9 +80,9 @@ public class MySQLConnection {
 			statement.setString(4, item.getImageUrl());
 			statement.setString(5, item.getUrl());
 			statement.executeUpdate();
-			
+
 			sql = "INSERT IGNORE INTO keywords VALUES (?, ?)";
-                    statement = conn.prepareStatement(sql);
+			statement = conn.prepareStatement(sql);
 			statement.setString(1, item.getItemId());
 			for (String keyword : item.getKeywords()) {
 				statement.setString(2, keyword);
@@ -93,7 +92,7 @@ public class MySQLConnection {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Set<String> getFavoriteItemIds(String userId) {
 		if (conn == null) {
 			System.err.println("DB connection failed");
@@ -117,7 +116,7 @@ public class MySQLConnection {
 
 		return favoriteItems;
 	}
-	
+
 	public Set<Item> getFavoriteItems(String userId) {
 		if (conn == null) {
 			System.err.println("DB connection failed");
@@ -169,5 +168,66 @@ public class MySQLConnection {
 			e.printStackTrace();
 		}
 		return keywords;
+	}
+
+	public String getFullname(String userId) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return "";
+		}
+		String name = "";
+		String sql = "SELECT first_name, last_name FROM users WHERE user_id = ? ";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				name = rs.getString("first_name") + " " + rs.getString("last_name");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean verifyLogin(String userId, String password) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+		String sql = "SELECT user_id FROM users WHERE user_id = ? AND password = ?";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			statement.setString(2, password);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+
+	public boolean addUser(String userId, String password, String firstname, String lastname) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+
+		String sql = "INSERT IGNORE INTO users VALUES (?, ?, ?, ?)";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			statement.setString(2, password);
+			statement.setString(3, firstname);
+			statement.setString(4, lastname);
+
+			return statement.executeUpdate() == 1;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
